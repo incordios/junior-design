8ef2369f8683cd81b1efc37f7d1dfc06370df74b
Nicolas Rios <69522766+incordios@users.noreply.github.com>
2022-11-08 11:45:42 -0500

    Update robot.py
    to disregard director connection

--- src/robot.py
+++ src/robot.py
@@ -18,21 +18,16 @@ PORT = # DIRECTOR LISTENING PORT
 LISTEN_PORT = # ROBOT LISTEN PORT
 
 if __name__ == "__main__":
-  print('Register with director...')
-
-  registration_request = create_request(ROBOT_NAME, "Register", LISTEN_PORT) 
-  initiate_connection(HOST, PORT, registration_request, libclient)
-
-  print('Finished registration, booting up server to listen...')
-
-  while True:
-    msg = listen_for_director(HOST, LISTEN_PORT, libserver)
-
-    print(msg)
-    if msg['value'] == 'break':
-      break
-
-    print('Main Loop recieved ', msg, ' so will start to do corresponding task')
-
-    # READ THE MESSAGE AND DO WHATEVER YOUR ROBOT WILL DO.
-
+  print('Register with director...')
+  registration_request = create_request(ROBOT_NAME, "Register", LISTEN_PORT) 
+  initiate_connection(DIRECTOR_HOST, PORT, registration_request, libclient)
+  print('Finished registration, booting up server to listen...')
+  
+  while True:
+    msg = listen_for_director('0.0.0.0', LISTEN_PORT, libserver)
+    print(msg)
+    if msg['value'] == 'break':
+      break
+      
+    print('Main Loop recieved ', msg, ' so will start to do corresponding task')
+    # READ THE MESSAGE AND DO WHATEVER YOUR ROBOT WILL DO.

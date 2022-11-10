fd27d150391638e34c752fe9fd5069adbf123d05
Nicolas Rios <69522766+incordios@users.noreply.github.com>
2022-11-10 14:23:43 -0500

    Update robot.py
    add test mode and skeleton code for director

--- src/robot.py
+++ src/robot.py
@@ -31,3 +31,39 @@ if __name__ == "__main__":
       
     print('Main Loop recieved ', msg, ' so will start to do corresponding task')
     # READ THE MESSAGE AND DO WHATEVER YOUR ROBOT WILL DO.
+    while True:
+        if GPIO.input(40) == 1:
+            speak("""if you can hear this
+            the audio system is working""")
+            sleep(1)
+            oscillate(2)
+            speak("""if the motors oscillated twice
+            the motor system is working""")
+            sleep(1)
+            speak("""lastly, the LEDs should start blinking""")
+            for _ in range(3):
+                sleep(1)
+                led.on()
+                sleep(1)
+                led.off()
+                sleep(1)
+        else:
+            # connect to director
+            
+            # wait for lines
+            
+            # Motor - adjust constants for arm
+            t1 = threading.Thread(target=oscillate, args=(5,))
+            t2 = threading.Thread(target=speak, args=(
+            """Who's ready for tea? We have all kinds! Earl Grey, English Breakfast
+            Chamomile, and iced. And birthdays are my favorite celebrations
+            so I knew I had to prepare accordingly.""",))
+            
+            t1.start()
+            t2.start()
+         
+            #join the threads
+            t1.join()
+            t2.join()
+
+            GPIO.cleanup()

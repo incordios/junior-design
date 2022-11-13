a1b8ddbfb55d66a935324d811753e44aee9e9f00
Nicolas Rios <69522766+incordios@users.noreply.github.com>
2022-11-12 20:11:01 -0500

    remove keyboard interrupt and allow local runs

--- src/robot.py
+++ src/robot.py
@@ -9,61 +9,73 @@ import threading
 from Protocol import libserver
 from Protocol import libclient
 
-from Utils.robotUtils import get_ipv4, create_request, listen_for_director, start_connection, initiate_connection
+from Utils.robotUtils import (
+    get_ipv4,
+    create_request,
+    listen_for_director,
+    start_connection,
+    initiate_connection,
+)
 
-ROBOT_NAME = "Tester"
-DIRECTOR_HOST = "143.215.188.102"
-PORT = 4456
+ROBOT_NAME = "Robot1"
+DIRECTOR_HOST = "127.0.0.1"
+PORT = 65432
 
-SELF_HOST = get_ipv4()
+SELF_HOST = "127.0.0.1"
 LISTEN_PORT = 8000
 
 if __name__ == "__main__":
-  print('Register with director...')
+    print("Register with director...")
 
-  registration_request = create_request(ROBOT_NAME, "Register", LISTEN_PORT) 
-  initiate_connection(DIRECTOR_HOST, PORT, registration_request, libclient)
+    registration_request = create_request(ROBOT_NAME, "Register", LISTEN_PORT)
+    initiate_connection(DIRECTOR_HOST, PORT, registration_request, libclient)
 
-  print('Finished registration, booting up server to listen...')
+    print("Finished registration, booting up server to listen...")
 
-  while True:
-    msg = listen_for_director(SELF_HOST, LISTEN_PORT, libserver)
+    while True:
+        msg = listen_for_director(SELF_HOST, LISTEN_PORT, libserver)
 
-    print(msg)
-    if msg['value'] == 'break':
-      break
+        print(msg)
+        if msg["value"] == "break":
+            break
 
-    print('Main Loop recieved ', msg, ' so will start to do corresponding task')
+        print("Main Loop recieved ", msg, " so will start to do corresponding task")
 
-    # READ THE MESSAGE AND DO WHATEVER YOUR ROBOT WILL DO.
-    if GPIO.input(40) == 1:
-        speak("""if you can hear this
-        the audio system and wifi is working""")
-        sleep(1)
-        oscillate(2)
-        speak("""if the motors oscillated twice
-        the motor system is working""")
-        sleep(1)
-        speak("""lastly, the L.E.D light should start blinking""")
-        for _ in range(3):
+        # READ THE MESSAGE AND DO WHATEVER YOUR ROBOT WILL DO.
+        if GPIO.input(40) == 1:
+            speak(
+                """if you can hear this
+        the audio system and wifi is working"""
+            )
             sleep(1)
-            led.on()
+            oscillate(2)
+            speak(
+                """if the motors oscillated twice
+        the motor system is working"""
+            )
             sleep(1)
-            led.off()
-            sleep(1)
-        speak("""flip the switch off to stop the test mode from
-        continuing""")
-        sleep(2)
-    else:
-        # Motor - adjust constants for arm
-        t1 = threading.Thread(target=oscillate, args=(5,))
-        t2 = threading.Thread(target=speak, args=(msg,))
-        
-        t1.start()
-        t2.start()
-     
-        #join the threads
-        t1.join()
-        t2.join()
+            speak("""lastly, the L.E.D light should start blinking""")
+            for _ in range(3):
+                sleep(1)
+                led.on()
+                sleep(1)
+                led.off()
+                sleep(1)
+            speak(
+                """flip the switch off to stop the test mode from
+        continuing"""
+            )
+            sleep(2)
+        else:
+            # Motor - adjust constants for arm
+            t1 = threading.Thread(target=oscillate, args=(5,))
+            t2 = threading.Thread(target=speak, args=(msg,))
 
-        GPIO.cleanup()
+            t1.start()
+            t2.start()
+
+            # join the threads
+            t1.join()
+            t2.join()
+
+            GPIO.cleanup()
